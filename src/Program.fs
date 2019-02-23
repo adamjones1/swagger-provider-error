b6d344a35a2de97ff1badc2b6c54cd32fc15b19d
Adam Jones <adam.jones@orbis.com>
2019-02-23 12:16:13 -0400

    minimal counterexample of issue

---
+++ src/Program.fs
@@ -0,0 +1,10 @@
+﻿open SwaggerProvider
+
+type MyTypeProvider = SwaggerProvider<"./spec.json">
+
+[<EntryPoint>]
+let main _ =
+    let provider = MyTypeProvider()
+    let response = provider.ResponseGet()
+    let items = response.Items
+    0
